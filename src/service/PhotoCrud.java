3d1361f61969d4ce206c18b8b3a58aefa03fe443
bintumustafa <65418892+bintumustafa@users.noreply.github.com>
2022-01-29 21:09:34 +0000

    Update PhotoCrud.java

--- src/service/PhotoCrud.java
+++ src/service/PhotoCrud.java
@@ -8,7 +8,7 @@ public class PhotoCrud {
 	public String createPhoto(String title,String description, String keywords, InputStream img, String nomAlb){
 		String message = null;
 		PhotoDao  p = new PhotoDao();
-		String pth = "mesImages/" +nomPho;
+		String pth = "mesImages/" +nomPho+ ".jpg"; //ou png
 		boolean created = p.add(title, pth, nomAlb); //les img sont stock�es dans un doc du projet et les chemins abs dans la bd
 		boolean added = add(img);//methode � impl�menter pour mettre l'img dans le doc mesImages
 		if (created && added)
@@ -21,9 +21,23 @@ public class PhotoCrud {
 		return false;
 	}
 
-	public String readPhoto(String title) {
-		DaoPhoto p = new DaoPhoto();
-		String path =p.select(nomPho);
+	public String readPhoto(Photo p) {
+		DaoPhoto dp = new DaoPhoto();
+		String path = dp.select(p.getId()); //à modifier
 		return path;
+	}
+	
+	public String updatePhoto(Photo p){
+		DaoPhoto dp = new DaoPhoto();
+	}
+	
+	public String deleteAlbum(Photo p){
+		String message = null;
+		DaoPhoto dp = new DaoPhoto();
+		boolean deleted = dp.delete(p);
+		if (deleted)
+			message = "La photo a �t� supprim�";		
+		return message;
+
 	}
 }
