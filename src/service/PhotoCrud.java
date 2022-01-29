5ed088b31d05e1aa17ff30738c566b2903e7d5d0
bintumustafa <65418892+bintumustafa@users.noreply.github.com>
2022-01-29 15:32:59 +0000

    Update PhotoCrud.java

--- src/service/PhotoCrud.java
+++ src/service/PhotoCrud.java
@@ -2,17 +2,17 @@ package service;
 
 import java.io.InputStream;
 
-import dao.DaoPhoto;
+import dao.PhotoDao;
  
 public class PhotoCrud {
-	public String createPhoto(String nomPho, InputStream img, String nomAlb){
+	public String createPhoto(String title,String description, String keywords, InputStream img, String nomAlb){
 		String message = null;
-		DaoPhoto p = new DaoPhoto();
+		PhotoDao  p = new PhotoDao();
 		String pth = "mesImages/" +nomPho;
-		boolean created = p.insert(nomPho, pth, nomAlb); //les img sont stock�es dans un doc du projet et les chemins abs dans la bd
+		boolean created = p.add(title, pth, nomAlb); //les img sont stock�es dans un doc du projet et les chemins abs dans la bd
 		boolean added = add(img);//methode � impl�menter pour mettre l'img dans le doc mesImages
 		if (created && added)
-			message = "La photo "+nomPho + "a �t� cr��";
+			message = "La photo "+title + "a �t� cr��";
 		return message;
 	}
 	
@@ -21,7 +21,7 @@ public class PhotoCrud {
 		return false;
 	}
 
-	public String readPhoto(String nomPho) {
+	public String readPhoto(String title) {
 		DaoPhoto p = new DaoPhoto();
 		String path =p.select(nomPho);
 		return path;
